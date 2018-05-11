f9616bc9e6c95feb647eeb18493b46aa7c901ccf
Tim Park <timfpark@gmail.com>
2018-05-11 09:43:42 -0400

    immediately fire interval

--- index.js
+++ index.js
@@ -7,18 +7,32 @@ class IntervalConnection extends DirectConnection {
         this.config.interval = this.config.interval || 60 * 60 * 1000;
     }
 
+    sendIntervalMessage() {
+        this.enqueue(
+            [
+                {
+                    body: {
+                        intervalStart: Date.now()
+                    }
+                }
+            ],
+            err => {
+                if (err)
+                    return console.log(
+                        'Error sending interval message: ' + err
+                    );
+            }
+        );
+    }
+
     start(callback) {
         super.start(err => {
             if (err) return callback(err);
 
+            this.sendIntervalMessage();
+
             setInterval(() => {
-                this.enqueue([{
-                    body: {
-                        intervalStart: Date.now()
-                    }
-                }], err => {
-                    if (err) return console.log("Error sending interval message: " + err);
-                });
+                this.sendIntervalMessage();
             }, this.config.interval);
 
             return callback();
